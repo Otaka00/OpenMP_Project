f679b0486ab57dca5bd42a4414b75d1a2a9afbfc
= <ahmad_otaca@hotmail.com>
2023-05-16 00:17:52 +0300

    Edit for scheduling

--- OpenMP_Project/OpenMP_Project.cpp
+++ OpenMP_Project/OpenMP_Project.cpp
@@ -18,7 +18,7 @@ int main(int argc, char** argv)
 
     namedWindow(windowTitle); // Create a window
 
-    imshow(windowTitle, image); 
+    imshow(windowTitle, image);
 
     int rows = image.rows;
     int cols = image.cols;
@@ -30,29 +30,30 @@ int main(int argc, char** argv)
     if (kernel_size % 2 == 0)
         kernel_size += 1;
     int border_size = kernel_size / 2;
-    
+
     std::cout << "Enter the number of threads: ";
     cin >> numThreads;
     omp_set_num_threads(numThreads);
     start_time = omp_get_wtime(); //Get start time of the parallel section
 
-    int chunkSize = ((pow(kernel_size,2)) * rows * cols) / numThreads;
+    int chunkSize = ((pow(kernel_size, 2)) * rows * cols) / numThreads;
     cout << "\nChunk Size: " << chunkSize;
 
     //Parallelize the outer loop, allowing multiple threads to process different rows of the image concurrently.
     //Iterate over the pixels (rows and cols) of the image 
+#pragma omp parallel for collapse(2) num_threads(numThreads) 
     for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols ; j++) {
-            
+        for (int j = 0; j < cols; j++) {
+
             cv::Vec3b& pixel = image.at<cv::Vec3b>(i, j);
 
-             // Perform low-pass filtering operation on the pixel with the Kernel size the user entered.
-            // Iterates over a kernel_sizexkernel_size kernel centered around the current pixel
+            // Perform low-pass filtering operation on the pixel with the Kernel size the user entered.
+           // Iterates over a kernel_sizexkernel_size kernel centered around the current pixel
             int blueSum = 0, greenSum = 0, redSum = 0, count = 0;
-#pragma omp parallel for num_threads(numThreads) schedule(dynamic, 512)
 
-            for (int k = 0; k < kernel_size; k++) {
-                for (int l = 0; l < kernel_size; l++) {
+#pragma omp parallel for collapse(2) num_threads(numThreads) reduction(+:greenSum, redSum, blueSum, count) 
+            for (int k = -border_size; k <= border_size; k++) {
+                for (int l = -border_size; l <= border_size; l++) {
 
                     int kernel_row = i + k;
                     int kernel_col = j + l;
@@ -67,11 +68,12 @@ int main(int argc, char** argv)
                 }
             }
             //Calculate the average value of the 3 channels in the current pixel
-                pixel[0] = blueSum / count;  // Blue channel
-                pixel[1] = greenSum / count;  // Green channel
-                pixel[2] = redSum / count;  // Red channel
+            pixel[0] = blueSum / count;  // Blue channel
+            pixel[1] = greenSum / count;  // Green channel
+            pixel[2] = redSum / count;  // Red channel
         }
     }
+
     end_time = omp_get_wtime(); //Get end time of the parallel section
     cv::imshow("Blurred Image", image);
 
@@ -79,10 +81,45 @@ int main(int argc, char** argv)
 
     //Wait for any keystroke in the window
     waitKey(0);
-    destroyAllWindows(); 
+    destroyAllWindows();
+
+    return 0;
+    
+}
+
+/*
+cv::Mat performConvolution(const cv::Mat& image, const cv::Mat& kernel) {
+    cv::Mat result;
+    cv::filter2D(image, result, -1, kernel);
+    return result;
+}
+
+int main() {
+    // Load the image
+    cv::Mat image = cv::imread("..\\lena.png", cv::IMREAD_GRAYSCALE);
+
+    // Define the kernel
+    cv::Mat kernel = (cv::Mat_<float>(7, 7) << -1, -1, -1, -1, -1, -1, -1,
+        -1, -1, -1, -1, -1, -1, -1,
+        -1, -1, -1, -1, -1, -1, -1,
+        -1, -1, -1, 49, -1, -1, -1,
+        -1, -1, -1, -1, -1, -1, -1,
+        -1, -1, -1, -1, -1, -1, -1,
+        -1, -1, -1, -1, -1, -1, -1);
+
+    // Perform convolution
+    cv::Mat convolvedImage = performConvolution(image, kernel);
+
+    // Display the original and convolved images
+    cv::imshow("Original Image", image);
+    cv::imshow("Convolved Image", convolvedImage);
+    cv::waitKey(0);
 
     return 0;
 }
+
+*/
+
 
 
 /*
